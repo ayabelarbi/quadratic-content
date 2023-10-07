45b4cf6a8b30f66d2b7ac2efaf21e9a8033f7442
Aya <aya.belarbi16@gmail.com>
2023-10-07 17:29:41 +0200

    grantify init

---
+++ grantify/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+        <div>
+          <h1> Grantify </h1>
+          <h1> Learn.Earn.Share</h1>
+        </div>
+      
+    </>
+  )
+}
+
+export default App
